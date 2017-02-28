001a6022d0633a8fde172b1a28d30260f19a5408
Karsten Weber <karstenweber1975@gmail.com>
2017-02-28 07:01:24 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+    var returnObj = {};
+    returnObj.ipaddress = req.headers['x-forwarded-for'];
+    var langArr = req.headers['accept-language'].split(";");
+    var langArrTwo = langArr[0].split(",");
+    returnObj.language = langArrTwo[0];
+    var softwareArr = req.headers['user-agent'].split("(");
+    var softwareArrTwo = softwareArr[1].split(")");
+    returnObj.software = softwareArrTwo[0];
+    res.json(returnObj);
+    
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The header_parser server is running ....");
+});
